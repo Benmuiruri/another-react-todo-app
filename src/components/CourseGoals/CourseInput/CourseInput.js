9ebdb670664d32f11c243e17c6bf56ed1bd182ce
Benmuiruri <ben.muiruri13@gmail.com>
2022-04-27 09:14:38 +0300

    Add styles dynamically based on user input

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,29 @@ import './CourseInput.css';
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true)
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
     setEnteredValue('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className='form-control'>
-        <label>Course Goal</label>
+      <div className={`form-control ${!isValid ? 'invalid': ''}`}>
+        <label >Course Goal</label>
         <input
           type='text'
           value={enteredValue}
